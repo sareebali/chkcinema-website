579547c21b97ae655f2b91bb5935bee2fbb787de
Syed Areeb Ali <alisyedareeb@gmail.com>
2024-01-18 12:23:51 +0800

    first commit

---
+++ JavaScript/movies.js
@@ -0,0 +1,64 @@
+function getMovies() {
+    return [
+        {
+            id:1,
+            type:"now",
+            thumbnail:"../Pictures/bigbuck.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+            name:"Big Buck Bunny",
+            cast:"Jack Black, Sabastian Stan, Amy Adams",
+            director:"Brad Bird",
+            duration: "1hour 32minutes"
+        },
+        {
+            id:2,
+            type:"now",
+            thumbnail:"../Pictures/wildlife.jpg",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+            name:"Chasing Dreams",
+            cast:"Eddie Redmayne, Jason Segel, Emma Stone",
+            director:"Christopher Nolan",
+            duration: "1hour 45minutes"
+        },
+        {
+            id:3,
+            type:"now",
+            thumbnail:"../Pictures/wonders.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+            name:"Eutopia",
+            cast:"Tom Cruise, Terry Crews, Elizabeth Olsen",
+            director:"Robert B. Weide",
+            duration: "2hour 10minutes"
+        },
+        {
+            id:4,
+            type:"upcoming",
+            thumbnail:"../Pictures/barbecue.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
+            name:"Hungry Harry",
+            cast:"Dwayne Johnson, Matthew Perry, Jennifer Lopez",
+            director:"Jordan Peele",
+            duration: "1hour 54minutes"
+        },
+        {
+            id:5,
+            type:"upcoming",
+            thumbnail:"../Pictures/nature.jpg",
+            src:"http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+            name:"Animal Dynasty",
+            cast:"Rami Malek, Morgan Freeman, Angelina Jolie",
+            director:"Quentin Tarantino",
+            duration: "1hour 40minutes"
+        },
+        {
+            id:6,
+            type:"upcoming",
+            thumbnail:"../Pictures/chocolate.jpg",
+            src:"https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+            name:"Cocoaland",
+            cast:"Will Smith, Kevin Hart, Kristen Stewart",
+            director:"Chris Columbus",
+            duration: "1hour 50minutes"
+        }
+    ]
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# chkcinema-website" 
